e1239770a67ba9e72472a4ec591877cece0d422a
AlenaKarpolenko <alenac190590@mail.ru>
2022-03-17 20:01:06 +0700

    added solution

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -16,27 +16,40 @@ function getArrayParams(arr) {
     sum += arr[i];
     }
     avg = Number((sum / arr.length).toFixed(2));
-    return { min: min, max: max, avg: avg };
+    return { min: min, max: max, avg: avg 
+    }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i=0; i<arr.length; i++){
+    sum = sum + arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  for (let i=0; i<arrOfArr.length; i++) {
+    let arrSum = func(arrOfArr[i]);
+    if (arrSum > max){
+      max = arrSum;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+let min = Infinity;
+let max = -Infinity;
+for (let i=0; i<arrOfArr.length; i++) {
+  if (arr[i]<min){
+    min = arr[i];
+  }
+  if (arr[i]>max){
+    max = arr[i];
+  }
+}
+return Math.abs(max-min);
 }

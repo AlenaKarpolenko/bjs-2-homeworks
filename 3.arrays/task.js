698668f389b5111722eb3eddef0099039c027baa
AlenaKarpolenko <alenac190590@mail.ru>
2022-03-20 19:18:54 +0700

    added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
+"use strict"
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
+ if (arr1.length !== arr2.length) {
+   result = false;
+ }
+else {
+  result = arr1.every((element, i) => element === arr2[i]);
+}
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  let arrNumbers = arr.filter((number) => number > 0 && number % 3 === 0); 
+  resultArr = arr.map((number) => number*10);
 
   return resultArr; // array
 }

f7ea5f3028413fccd1ffc40ce4b05ba371e2557b
AlenaKarpolenko <alenac190590@mail.ru>
2022-03-30 12:25:20 +0700

    added

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,52 @@
+'use strict'
+
+function parseCount(parseNumber) {
+    let number = Number.parseInt(parseNumber);
+    if (isNaN(number)) {
+        throw new Error('Невалидное число');
+    }
+    return number;
+}
+
+function validateCount(unParseNumber) {
+    try {
+        return parseCount(unParseNumber);
+    } catch (error) {
+        return error;
+    }
+}
+
+
+//треугольник
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    getPerimeter() {
+        return this.a + this.b + this.c
+    }
+    getAria() {
+        let p = this.getPerimeter / 2;
+        return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch {
+        return {
+            getArea() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            getPerimeter() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        };
+    }
+}

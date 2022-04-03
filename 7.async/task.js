a3793db8e94b7d8706d3ae52fab4f2332de21b14
AlenaKarpolenko <alenac190590@mail.ru>
2022-04-03 21:00:00 +0700

    внесла поправки

--- 7.async/task.js
+++ 7.async/task.js
@@ -4,10 +4,11 @@ class AlarmClock {
         this.timerId = null;
     }
     addClock(time, callback, id) {
-        if (typeof id === 'undefined') {
+        if (isNaN(id)) {
             throw new Error('id таймера не передан');
-        } else if (typeof this.alarmCollection.find(clock => clock.id === id) !== 'undefined') {
-            return console.error('звонок существует');
+        } else if (this.alarmCollection.some((clock) => clock.id == id)) {
+            console.error('звонок существует');
+            return;
         }
         this.alarmCollection.push({
             time: time,
@@ -19,59 +20,57 @@ class AlarmClock {
         let arrLength = this.alarmCollection.length;
         this.alarmCollection = this.alarmCollection.filter(
             clock => clock.id !== id);
-        let outArrLength = this.alarmCollection.length;
-        return outArrLength < arrLength;
-    }
-    getCurrentFormattedTime() {
-        let date = new Date();
-        let hourse = date.getHours();
-        let minutes = date.getMinutes();
-
-        if (date.getHours() < 10) {
-            hourse = '0' + date.getHours();
-        }
-        if (date.getMinutes() < 10) {
-            minutes = '0' + date.getMinutes
-        }
-        return hourse + ':' + minutes;
+        return this.alarmCollection.length < arrLength;
     }
 
-    start() {
-        if (!this.timer) {
-            this.timer = setInterval(() => {
-                this.alarmCollection.forEach(clock => {
-                    if (clock.time == this.getCurrentFormattedTime()) {
-                        clock.callback();
-                    }
-                });
+
+    
+
+
+start() {
+    if (!this.timer) {
+        this.timer = setInterval(() => {
+            this.alarmCollection.forEach(clock => {
+                if (clock.time == this.getCurrentFormattedTime()) {
+                    clock.callback();
+                }
             });
-        }
+        });
     }
+}
 
-    stop() {
-        if (this.timer !== null) {
-            clearInterval(this.timer);
-            return this.timer = null;
-        }
+stop() {
+    if (this.timer !== null) {
+        clearInterval(this.timer);
+        this.timer = null;
     }
-    printAlarms() {
-        return this.alarmCollection.forEach(clock => console.log(clock.id + ': ' + clock.time));
-    }
-    clearAlarms() {
-        this.stop();
-        return this.alarmCollection = [];
-    }
+}
+printAlarms() {
+    console.log ('Количество будильников:' + this.alarmCollection.length);
+    this.alarmCollection.forEach(clock => console.log(clock.id + ': ' + clock.time));
+}
+clearAlarms() {
+    this.stop();
+   this.alarmCollection = [];
+}
+
+
+getCurrentFormattedTime(){
+    return new Date().toLocaleTimeString("ru-Ru", {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+
+}
 }
 
 
 
-
-
-let PhoneClock = new AlarmClock();
-PhoneClock.addClock('08:01', () => console.log('пора вствать'), 1);
-PhoneClock.addClock('08:02', () => console.log('вствай, а то проспишь'), 2);
-PhoneClock.removeClock(2);
-PhoneClock.addClock('08:03', () => console.log('вставай уже'), 3);
-PhoneClock.start();
-PhoneClock.stop();
-PhoneClock.printAlarms();
+let phoneClock = new AlarmClock();
+phoneClock.addClock('08:01', () => console.log('пора вствать'), 1);
+phoneClock.addClock('08:02', () => console.log('вствай, а то проспишь'), 2);
+phoneClock.removeClock(2);
+phoneClock.addClock('08:03', () => console.log('вставай уже'), 3);
+phoneClock.start();
+phoneClock.stop();
+phoneClock.printAlar
